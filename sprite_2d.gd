8376ecd84664369aa31ab30d97f7b928197f20fa
Rafael Alvarez <cafepraestudar@gmail.com>
2026-05-07 11:11:51 -0300

    Implementação de encaixe funcional

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -1,8 +1,11 @@
 extends Sprite2D
 
 var arrastando = false
+var encaixado = false
 
 func _input(event):
+	if encaixado:
+		return #Objeto destravado
 	# Evento toque/clique
 	if event is InputEventMouseButton or event is InputEventScreenTouch:
 		if event.pressed: # Touch Down
@@ -16,6 +19,7 @@ func _input(event):
 			if global_position.distance_to(alvo.global_position) < 100:
 			# 3. O robô "pula" para a posição exata do alvo
 				global_position = alvo.global_position
+				encaixado = true # Objeto travado
 			
 		# Evento mover mouse/toque
 	if event is InputEventMouseMotion or event is InputEventScreenDrag:
