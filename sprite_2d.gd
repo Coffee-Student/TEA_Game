a6a3b609f80449dba22a8e8e71f45cc40278976e
Rafael Alvarez <cafepraestudar@gmail.com>
2026-05-07 10:51:41 -0300

    Implementação lógica de encaixe por distância

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -5,16 +5,19 @@ var arrastando = false
 func _input(event):
 	# Evento toque/clique
 	if event is InputEventMouseButton or event is InputEventScreenTouch:
-		# Touch Down
-		if event.pressed:
-			# Verifica toque "touch down"
+		if event.pressed: # Touch Down
 			if get_rect().has_point(to_local(event.position)):
 				arrastando = true
 		else:
-			# Soltou o toque "touch up"
-			arrastando = false
+			arrastando = false # Touch up
+			# 1. Localiza o nó do Alvo na cena
+			var alvo = get_parent().get_node("Alvo")
+			# 2. Se a distância até o alvo for menor que 100 pixels...
+			if global_position.distance_to(alvo.global_position) < 100:
+			# 3. O robô "pula" para a posição exata do alvo
+				global_position = alvo.global_position
 			
-	# Arrastando para se mover
+		# Evento mover mouse/toque
 	if event is InputEventMouseMotion or event is InputEventScreenDrag:
 		if arrastando:
 			# Movimentação
